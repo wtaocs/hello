43b76e9050e8d4b3a1b0b502f4a34ee62b099539
ASUS <ASUS@china>
2016-07-19 20:06:16 +0800

    又加了一个方法

--- src/com/wtao/hello/App.java
+++ src/com/wtao/hello/App.java
@@ -10,6 +10,12 @@ public class App {
 	public static void main(String[] args) {
 		sayHello("github");
 		haha();
+		haha2();
+	}
+
+	private static void haha2() {
+		// TODO Auto-generated method stub
+		System.out.println("666");
 	}
 
 	private static void haha() {
