4b32bdaf909d222618dd4b7c8d837c9603595e4d
Administrator <Administrator@2013-20150320EH>
2016-07-19 21:02:57 +0800

    添加了一个readHello的类

--- src/com/wtao/hello/App.java
+++ src/com/wtao/hello/App.java
@@ -9,9 +9,14 @@ public class App {
 
 	public static void main(String[] args) {
 		sayHello("github");
+		readHello();
 	}
 
 	private static void sayHello(String msg) {
 		System.out.println("hello " + msg);
+	}
+	
+	private static void readHello() {
+		System.out.println("hello");
 	}
 }
