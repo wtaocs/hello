e87f5f40a6a13cf713945d989d613809039cd5b9
六 <@DESKTOP-4LMNP0D>
2016-07-19 20:07:20 +0800

    修改

---
+++ src/com/wtao/hello/wukaijie.java
@@ -0,0 +1,7 @@
+package com.wtao.hello;
+
+public class wukaijie {
+       public static void main(String[] args) {
+		 System.out.println("holle,can you tell me your QQ number and phone number?");
+	}
+}
